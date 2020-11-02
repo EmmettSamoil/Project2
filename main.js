4903a6e93d02230213637c4bab634fdd7195710e
EmmettSamoil <EmmettSamoil@users.noreply.github.com>
2020-11-02 13:58:01 -0700

    a
    a

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let productList = [];
 let cartEl = document.getElementById('cartOutput');
 document.getElementById('purchase1').addEventListener('click', addAlbum);
 document.getElementById('purchase2').addEventListener('click', addAlbum);
-document.getElementById('purchase3').addEventListener('click', addAlbum)
+document.getElementById('purchase3').addEventListener('click', addAlbum);
 
 function addAlbum(event) {
     let albumName = event.target.dataset.name;
@@ -10,7 +10,7 @@ function addAlbum(event) {
 
     document.getElementById('cartItems').innerHTML = '';
     for (let i = 0; i < productList.length; i++) {
-        document.getElementById('cartItems').innerHTML += '<p>' + productList[i].name + '</a> <span class="price">$5</span></p>';
+        document.getElementById('cartItems').innerHTML += '<p>' + productList[i].name + ' <button id="remove">Remove<button></a> <span class="price">$5</span></p>';
         document.getElementById('cartAmount').innerHTML = [i + 1];
         document.getElementById('cartCost').innerHTML = [i + 1] * 5
     }
