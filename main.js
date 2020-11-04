3d561d94efc84e507e10a89274f3daf7b612bc28
EmmettSamoil <EmmettSamoil@users.noreply.github.com>
2020-11-03 21:22:44 -0700

    update
    fixed rthe thing

--- main.js
+++ main.js
@@ -6,14 +6,27 @@ document.getElementById('purchase3').addEventListener('click', addAlbum);
 
 function addAlbum(event) {
     let albumName = event.target.dataset.name;
-    productList.push(addProduct(albumName));
+    
+    let alreadyThere = false;
+    for(let i = 0; i < productList.length; i++){
+        if(productList[i].name == albumName){
+            alreadyThere = true;
+        }
+    }
+    if(alreadyThere == false){
+        productList.push(addProduct(albumName));
+    }
 
     document.getElementById('cartItems').innerHTML = '';
     for (let i = 0; i < productList.length; i++) {
         document.getElementById('cartItems').innerHTML += '<p>' + productList[i].name + ' <button id="remove">Remove<button></a> <span class="price">$5</span></p>';
         document.getElementById('cartAmount').innerHTML = [i + 1];
-        document.getElementById('cartCost').innerHTML = [i + 1] * 5
+        document.getElementById('cartCost').innerHTML = [i + 1] * 5;
     }
+}
+
+function removeAlbum(albumName) {
+
 }
 
 function addProduct(initName) {
