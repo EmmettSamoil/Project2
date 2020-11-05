85e8918b86bb22998e10e5133845076c840d44fb
EmmettSamoil <EmmettSamoil@users.noreply.github.com>
2020-11-05 14:22:56 -0700

    remove button
    remove button

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let cartEl = document.getElementById('cartOutput');
 document.getElementById('purchase1').addEventListener('click', addAlbum);
 document.getElementById('purchase2').addEventListener('click', addAlbum);
 document.getElementById('purchase3').addEventListener('click', addAlbum);
+document.addEventListener('click', clickHandler);
 
 function addAlbum(event) {
     let albumName = event.target.dataset.name;
@@ -19,14 +20,29 @@ function addAlbum(event) {
 
     document.getElementById('cartItems').innerHTML = '';
     for (let i = 0; i < productList.length; i++) {
-        document.getElementById('cartItems').innerHTML += '<p>' + productList[i].name + ' <button id="remove">Remove<button></a> <span class="price">$5</span></p>';
+        document.getElementById('cartItems').innerHTML += "<p>" + productList[i].name + " <button id= '" + productList[i].name + "'>Remove<button></a> <span class='price'>$5</span></p>";
         document.getElementById('cartAmount').innerHTML = [i + 1];
         document.getElementById('cartCost').innerHTML = [i + 1] * 5;
     }
 }
 
-function removeAlbum(albumName) {
-
+function clickHandler(event) {
+    if (event.target.nodeName == 'BUTTON') {
+        let targetName = event.target.dataset.name;
+        for (let i = 0; i < productList.length; i++) {
+            if (productList[i].albumName == targetName) {
+                // document.getElementById('cartItems').innerHTML = '';
+                productList.splice(i, 1);
+                break;
+            }
+        }
+    }
+    document.getElementById('cartItems').innerHTML = '';
+    for (let i = 0; i < productList.length; i++) {
+        document.getElementById('cartItems').innerHTML += "<p>" + productList[i].name + " <button id= '" + productList[i].name + "'>Remove<button></a> <span class='price'>$5</span></p>";
+        document.getElementById('cartAmount').innerHTML = [i + 1];
+        document.getElementById('cartCost').innerHTML = [i + 1] * 5;
+    }
 }
 
 function addProduct(initName) {
